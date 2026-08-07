5e7518a37d7a738207b2c694b73e84d520d6a120
andresflorez16 <andresflorez.dev@gmail.com>
2026-08-06 21:04:28 -0500

    first commit

---
+++ src/data/invitation.ts
@@ -0,0 +1,13 @@
+export const invitation = {
+  name: "Juliana Silva",
+  title: "Mis Quince Años",
+  message:
+    "Te invito a celebrar conmigo una noche muy especial llena de sueños y alegría.",
+  day: "SÁBADO",
+  date: "15",
+  month: "NOVIEMBRE",
+  time: "7:00 PM",
+  venue: "Salón de Eventos Borcelle",
+  address: "Calle Cualquiera 123",
+  city: "Ciudad",
+};
